59631ac7468299e3dbd542e379a4d438919c1fef
augustinbarillec <augustin.barillec@easyence.com>
2022-09-04 14:41:24 +0200

    wip

--- tests/params.py
+++ tests/params.py
@@ -3,8 +3,8 @@ from copy import deepcopy
 team_params = {
     'max_guessers_per_game': 20,
     'max_life_span': 3600,
-    'max_running_games': 100,
-    'max_running_games_per_organizer': 100,
+    'max_running_games': 5,
+    'max_running_games_per_organizer': 1,
     'refresh_interval': 4,
     'self_trigger_threshold': 60,
     'tagging': False,
@@ -14,5 +14,7 @@ team_params = {
 
 channel_default_params = deepcopy(team_params)
 channel_default_params['max_guessers_per_game'] = 3
+channel_default_params['max_running_games'] = 100
+channel_default_params['max_running_games_per_organizer'] = 100
 channel_default_params['refresh_interval'] = 9
 channel_default_params['tagging'] = True
