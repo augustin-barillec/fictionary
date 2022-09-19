936bc772d38a3ac3a5666ca28c941e1ef02e494d
augustinbarillec <augustin.barillec@easyence.com>
2022-09-19 20:41:38 +0200

    wip

--- cloudbuild/write.py
+++ cloudbuild/write.py
@@ -49,12 +49,10 @@ parallelizable_sources = [
 
 no_parallelizable_sources = []
 parallelizable_sources = [
-    'endings/hey.cy.js',
     'endings/no_votes.cy.js',
     'endings/well.cy.js',
     'endings/zero.cy.js',
-    'exceptions/vote_click/not_a_potential_voter.cy.js',
-    'exceptions/vote_submission/no_time_left.cy.js'
+    'exceptions/vote_click/not_a_potential_voter.cy.js'
 ]
 
 
