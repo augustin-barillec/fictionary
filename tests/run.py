a18d02cceef492a76b0e0a6236b959e8263688c6
augustinbarillec <augustin.barillec@easyence.com>
2022-11-13 19:31:36 +0100

    wip

--- tests/run.py
+++ tests/run.py
@@ -64,6 +64,26 @@ last_sources = [
     'help.cy.js',
     'special_characters.cy.js'
 ]
+first_sources = [
+
+]
+last_sources = [
+    'endings/hey.cy.js',
+    'endings/no_guesses.cy.js',
+    'endings/no_votes.cy.js',
+    'endings/thanks.cy.js',
+    'endings/winner.cy.js',
+    'exceptions/game_dead/view_response.cy.js',
+    'exceptions/guess_submission/max_these_guessers.cy.js',
+    'exceptions/pick_submission/not_integer.cy.js',
+    'exceptions/slash_command/max_this_running.cy.js',
+    'exceptions/vote_click/not_a_potential_voter.cy.js',
+    'setups/french.cy.js',
+    'transitions/guess_full_time.cy.js',
+    'transitions/guess_shorten_time.cy.js',
+    'transitions/vote_full_time.cy.js'
+]
+
 sources = first_sources + last_sources
 utils.local_clean.clean_cypress()
 logger.info('Deleting games...')
