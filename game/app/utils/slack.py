fbe62089c20adbf9af6cb2deb820302428842d41
augustinbarillec <augustin.barillec@easyence.com>
2022-11-30 22:52:37 +0100

    wip

--- game/app/utils/slack.py
+++ game/app/utils/slack.py
@@ -24,6 +24,7 @@ def multi_tries(f):
             except Exception as e:
                 logger.warning(e, stack_info=True)
                 time.sleep(duration)
+        return f(*args, **kwargs)
     return decored
 
 
