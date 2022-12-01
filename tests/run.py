cc0b13de328c06bddf2f3d7d6f6ae95da7cc48f6
augustinbarillec <augustin.barillec@easyence.com>
2022-12-02 00:02:40 +0100

    wip

--- tests/run.py
+++ tests/run.py
@@ -61,6 +61,30 @@ last_sources = [
     'help.cy.js',
     'special_characters.cy.js'
 ]
+first_sources = [
+    'small.cy.js',
+    'exceptions/slash_command/max_running.cy.js',
+    'exceptions/setup_submission/max_running.cy.js'
+]
+last_sources = [
+    'endings/hey.cy.js',
+    'endings/no_votes.cy.js',
+    'endings/thanks.cy.js',
+    'endings/well.cy.js',
+    'endings/winners.cy.js',
+    'exceptions/game_dead/update_view.cy.js',
+    'exceptions/pick_submission/not_between.cy.js',
+    'exceptions/pick_submission/not_integer.cy.js',
+    'exceptions/slash_command/not_invited.cy.js',
+    'exceptions/vote_click/not_a_potential_voter.cy.js',
+    'exceptions/vote_submission/no_time_left.cy.js',
+    'help.cy.js',
+    'setups/freestyle.cy.js',
+    'setups/french.cy.js',
+    'special_characters.cy.js',
+    'transitions/guess_full_time.cy.js',
+    'transitions/vote_full_time.cy.js'
+]
 sources = first_sources + last_sources
 len_sources = len(sources)
 utils.local_clean.clean_cypress()
