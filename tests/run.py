98d8b8d40236cfb5e19a6ddd30c631fb706f4bf1
augustinbarillec <augustin.barillec@gmail.com>
2022-12-03 21:19:01 +0100

    wip

--- tests/run.py
+++ tests/run.py
@@ -62,19 +62,10 @@ last_sources = [
     'special_characters.cy.js'
 ]
 first_sources = [
-    'exceptions/slash_command/max_running.cy.js',
-    'exceptions/setup_submission/max_running.cy.js'
 ]
 last_sources = [
-    'endings/bravo.cy.js',
-    'endings/zero.cy.js',
-    'exceptions/setup_submission/max_this_running.cy.js',
     'exceptions/slash_command/not_invited.cy.js',
-    'exceptions/vote_click/not_a_potential_voter.cy.js',
-    'exceptions/vote_submission/no_time_left.cy.js',
-    'setups/english.cy.js',
-    'special_characters.cy.js',
-    'transitions/vote_shorten_time.cy.js'
+    'exceptions/vote_click/not_a_potential_voter.cy.js'
 ]
 sources = first_sources + last_sources
 len_sources = len(sources)
