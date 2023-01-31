6940fe8eae92e8a13368da2bc58d8f87a3c1edcc
augustinbarillec <augustin.barillec@gmail.com>
2023-01-31 21:02:53 +0100

    wip

--- tests/cypress/e2e/exceptions/slash_command/invalid_parameter.cy.js
+++ tests/cypress/e2e/exceptions/slash_command/invalid_parameter.cy.js
@@ -5,7 +5,7 @@ describe('main', () => {
         cy.login_from_user_index(conf, 0)
         cy.go_to_channel_from_channel_id(conf, channel_id)
         cy.slash_command('tag', 'toto')
-        cy.contains('Game parameter must be one of help, freestyle, english or french.')
+        cy.contains('Parameter must be one of help, freestyle, english or french.')
       })
     })
   })
